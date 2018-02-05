42ad2832368facc1fc41cb2f84a2756e4aef4806
Yahor Skrabkou <Yahor_Skrabkou@epam.com>
2018-02-05 11:28:54 -0800

    change

--- library/buildAgentProperties.py
+++ library/buildAgentProperties.py
@@ -4,10 +4,25 @@ from ansible.module_utils.urls import *
 import os
 
 
+def register(params):
+
+  os.system(command)
+  has_changed = True
+
+  command = "ls"
+  meta = {"params:": command}
+  return (has_changed, meta)
+
+
+
 def main():
     fields = {
         "package": {"required": True, "type": "str"},
     }
+
+    module = AnsibleModule(argument_spec=fields)
+    has_changed, result = register(module.params)
+    module.exit_json(changed=has_changed, meta=result)
 
 
 if __name__ == "__main__":
